3be0567e21db1610d5eac106415727e545c822d8
Renée Kooi <renee@kooi.me>
2017-08-12 12:37:43 +0200

    Add module.exports test

---
+++ test/module-exports/app.js
@@ -0,0 +1,5 @@
+// Should be kept because this is the entry point.
+module.exports = {
+  a: require('./a'),
+  b: require('./b').b
+}
---
+++ test/module-exports/b.js
@@ -0,0 +1,5 @@
+module.exports = {
+  a: 1,
+  b: 2, // only this should be kept
+  c: 3
+}
