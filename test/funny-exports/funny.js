6dfe06cbc92d447296456302fb3ef083e100f779
dy <df.creative@gmail.com>
2019-01-11 13:15:15 -0500

    Add multiple exports test case

--- test/funny-exports/funny.js
+++ test/funny-exports/funny.js
@@ -5,4 +5,4 @@ console.log(exports.a = 'hello world')
 function a() {} exports.b = function () { return a }, console.log(a)
 var c = (0, exports.c = 'beep boop')
 
-exports.d = exports.e = null
+exports.d = exports.e = exports.f = exports.g = null
