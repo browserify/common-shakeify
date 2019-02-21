ff5162a30909ab2a913063a006f376ebed6fdcee
Timothy Johnson <timothy@idioticdev.com>
2019-02-21 10:40:32 -0800

    Don't shake when onExportDelete returns false

---
+++ test/exclude/expected.js
@@ -0,0 +1,14 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+exports.a = 1
+exports.b = 2
+/* common-shake removed: exports.c = */ void 3
+/* common-shake removed: exports.d = */ void 4
+/* common-shake removed: exports.e = */ void 5
+/* common-shake removed: exports.f = */ void 6
+/* common-shake removed: exports.g = */ void 7
+/* common-shake removed: exports.h = */ void 8
+
+},{}],2:[function(require,module,exports){
+console.log(require('./a').a)
+
+},{"./a":1}]},{},[2]);
