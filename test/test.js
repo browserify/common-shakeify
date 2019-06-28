d42e6db295f61e9d222b9f8b9657daa2dec479e5
Renée Kooi <renee@kooi.me>
2019-06-28 12:08:08 +0200

    move new dash-r test to its own folder

--- test/test.js
+++ test/test.js
@@ -140,23 +140,22 @@ test('dash-r', function (t) {
 
 test('dash-r node_modules', function (t) {
   var b = browserify({
-    entries: path.join(__dirname, 'dash-r/app2.js')
+    entries: path.join(__dirname, 'dash-r-node-modules/app.js')
   })
-  // using wrap-comment because it already exists in this repo
-  b.require('wrap-comment', { expose: 'net' })
+  b.require('net-browserify-stub', { expose: 'net' })
   b.plugin(commonShake)
 
   var bundle = b.bundle()
   bundle.on('error', t.fail)
 
   bundle.pipe(fs.createWriteStream(
-    path.join(__dirname, 'dash-r/actual2.js')
+    path.join(__dirname, 'dash-r-node-modules/actual.js')
   ))
 
   bundle.pipe(concat(function (result) {
     t.is(
       result.toString('utf8'),
-      fs.readFileSync(path.join(__dirname, 'dash-r/expected2.js'), 'utf8'),
+      fs.readFileSync(path.join(__dirname, 'dash-r-node-modules/expected.js'), 'utf8'),
       'dash-r'
     )
     t.end()
